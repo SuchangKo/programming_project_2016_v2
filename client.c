a03c2de88065753976b1d95044729f9498544ac9
youjinwook <nugisquare@gmail.com>
2016-06-08 04:24:29 +0900

    merge fundamental view

--- client.c
+++ client.c
@@ -17,43 +17,43 @@
 #define  FLOOR_MIN 0
 
 /*
- * Protocol
- * length : 3Byte 
- * Author : Suchang Ko
- * Date   : 2016.06.08
- * ----------------
- * 1. 상/하 이동
- * [0]Byte : 1
- * [1]Byte : 현재 층 수
- * [2]Byte : 목적 층 수
- * 비고 : 목적 층 수는 0층(지하1층)~10층 범위에서 Random.
- * ----------------
- * 2. 시작/일시정지
- * [0]Byte : 2
- * [1]Byte : Not Used
- * [2]Byte : Not Used
- * 비고 : 시작상태에서 이 명령이 들어오면 일시정지, 
- * 일시정지 상태에서 이 명령이 들어오면 시작
- * ----------------
- * 3. 속도 조절
- * [0]Byte : 3
- * [1]Byte : 0 -> 0.5s | 1 -> 1s | 2 -> 2s 
- * [2]Byte : Not Used
- * 비고 : 없음
- * ----------------
- * 4. 수리
- * [0]Byte : 4
- * [1]Byte : Not Used
- * [2]Byte : Not Used
- * 비고 : 수리 명령을 받으면 Server측의 현재 고장도 초기화
- * ----------------
- * 5. 종료
- * [0]Byte : 5
- * [1]Byte : Not Used
- * [2]Byte : Not Used
- * 비고 : 프로그램을 종료한다.
- * ----------------
- */
+* Protocol
+* length : 3Byte 
+* Author : Suchang Ko
+* Date   : 2016.06.08
+* ----------------
+* 1. 상/하 이동
+* [0]Byte : 1
+* [1]Byte : 현재 층 수
+* [2]Byte : 목적 층 수
+* 비고 : 목적 층 수는 0층(지하1층)~10층 범위에서 Random.
+* ----------------
+* 2. 시작/일시정지
+* [0]Byte : 2
+* [1]Byte : Not Used
+* [2]Byte : Not Used
+* 비고 : 시작상태에서 이 명령이 들어오면 일시정지, 
+* 일시정지 상태에서 이 명령이 들어오면 시작
+* ----------------
+* 3. 속도 조절
+* [0]Byte : 3
+* [1]Byte : 0 -> 0.5s | 1 -> 1s | 2 -> 2s 
+* [2]Byte : Not Used
+* 비고 : 없음
+* ----------------
+* 4. 수리
+* [0]Byte : 4
+* [1]Byte : Not Used
+* [2]Byte : Not Used
+* 비고 : 수리 명령을 받으면 Server측의 현재 고장도 초기화
+* ----------------
+* 5. 종료
+* [0]Byte : 5
+* [1]Byte : Not Used
+* [2]Byte : Not Used
+* 비고 : 프로그램을 종료한다.
+* ----------------
+*/
 
 int mygetch(void)
 {
@@ -81,7 +81,7 @@ int   main( int argc, char **argv)
 
 
    sock  = socket( PF_INET, SOCK_DGRAM, 0);
-   
+
    if( -1 == sock)
    {
       printf( "socket 생성 실패n");
@@ -95,7 +95,7 @@ int   main( int argc, char **argv)
    while(1){
       int cmd;
       char cmd_msg[3] = {0,0,0};
-      
+
       printf("=================\n");
       printf("[명령어를 입력해주세요]\n");
       printf("[1] 이동\n");
@@ -107,76 +107,76 @@ int   main( int argc, char **argv)
       printf("[INPUT] : ");
       scanf("%d",&cmd);
       switch(cmd){
-         case 1:{ // 상하 이동
-            int current_floor, target_floor;
-            int direction;
-            printf("현재 몇 층에 있나요? (지하1층 -> 0 입력) : ");
-            scanf("%d",&current_floor);
-            printf("올라가기[1] 내려가기[2] : ");
-            scanf("%d",&direction);
-            
-            srand(time(NULL));
+      case 1:{ // 상하 이동
+         int current_floor, target_floor;
+         int direction;
+         printf("현재 몇 층에 있나요? (지하1층 -> 0 입력) : ");
+         scanf("%d",&current_floor);
+         printf("올라가기[1] 내려가기[2] : ");
+         scanf("%d",&direction);
 
-            if(direction == UP){
-               if(current_floor == FLOOR_MAX){
-                  printf("여기가 끝층이야 못올라가\n");
-               }else{
-                  target_floor = current_floor + rand()%(FLOOR_MAX - current_floor) + 1;
-               }
-            }else if(direction == DOWN){
-               if(current_floor == FLOOR_MIN){
-                  printf("여기가 끝층이야 못내려가\n");
-               }else{
-                  target_floor = current_floor - rand()%(current_floor - FLOOR_MIN) - 1;
-               }
-            }
+         srand(time(NULL));
 
-            printf("[%d층] -> [%d층] 으로 이동\n",current_floor,target_floor);
-            if(current_floor > target_floor){
-               printf("내려갑니다\n");   
-            }else if(current_floor < target_floor){
-               printf("올라갑니다\n");   
+         if(direction == UP){
+            if(current_floor == FLOOR_MAX){
+               printf("여기가 끝층이야 못올라가\n");
+            }else{
+               target_floor = current_floor + rand()%(FLOOR_MAX - current_floor) + 1;
             }
-            
-            cmd_msg[0] = 1;
-            cmd_msg[1] = current_floor;
-            cmd_msg[2] = target_floor;
-            break;
+         }else if(direction == DOWN){
+            if(current_floor == FLOOR_MIN){
+               printf("여기가 끝층이야 못내려가\n");
+            }else{
+               target_floor = current_floor - rand()%(current_floor - FLOOR_MIN) - 1;
+            }
          }
-         case 2:{ // 시작 일시정지 
-            cmd_msg[0] = 2;
-            cmd_msg[1] = 1; //Not Used
-            cmd_msg[2] = 0; //Not Used
-            break;
+
+         printf("[%d층] -> [%d층] 으로 이동\n",current_floor,target_floor);
+         if(current_floor > target_floor){
+            printf("내려갑니다\n");   
+         }else if(current_floor < target_floor){
+            printf("올라갑니다\n");   
          }
-         case 3:{ // 속도 조절
-            int target_speed;
-            printf("1 : 0.5sec | 2 : 1sec | 3 : 2sec\n");
-            printf("엘레베이터 속도를 입력하세요 : ");
-            scanf("%d",&target_speed); 
-            cmd_msg[0] = 3;
-            cmd_msg[1] = target_speed; 
-            cmd_msg[2] = 0; //Not Used
-            break;
-         }
-         case 4:{ // 수리 
-            cmd_msg[0] = 4;
-            cmd_msg[1] = 0; //Not Used
-            cmd_msg[2] = 0; //Not Used
-            break;
-         }
-         case 5:{ // 종료
-            cmd_msg[0] = 5; 
-            cmd_msg[1] = 0; //Not Used
-            cmd_msg[2] = 0; //Not Used
-            break;
+
+         cmd_msg[0] = 1;
+         cmd_msg[1] = current_floor;
+         cmd_msg[2] = target_floor;
+         break;
+      }
+      case 2:{ // 시작 일시정지 
+         cmd_msg[0] = 2;
+         cmd_msg[1] = 1; //Not Used
+         cmd_msg[2] = 0; //Not Used
+         break;
+      }
+      case 3:{ // 속도 조절
+         int target_speed;
+         printf("1 : 0.5sec | 2 : 1sec | 3 : 2sec\n");
+         printf("엘레베이터 속도를 입력하세요 : ");
+         scanf("%d",&target_speed); 
+         cmd_msg[0] = 3;
+         cmd_msg[1] = target_speed; 
+         cmd_msg[2] = 0; //Not Used
+         break;
+      }
+      case 4:{ // 수리 
+         cmd_msg[0] = 4;
+         cmd_msg[1] = 0; //Not Used
+         cmd_msg[2] = 0; //Not Used
+         break;
+      }
+      case 5:{ // 종료
+         cmd_msg[0] = 5; 
+         cmd_msg[1] = 0; //Not Used
+         cmd_msg[2] = 0; //Not Used
+         break;
          }
       }
 
 
 
       sendto( sock, &cmd_msg, strlen(&cmd_msg)+1, 0,    // +1: NULL까지 포함해서 전송
-               ( struct sockaddr*)&server_addr, sizeof( server_addr));
+         ( struct sockaddr*)&server_addr, sizeof( server_addr));
       if(cmd == 5){
          return 0;
       }
@@ -184,11 +184,11 @@ int   main( int argc, char **argv)
       /*
       server_addr_size  = sizeof( server_addr);
       recvfrom( sock, buff_rcv, BUFF_SIZE, 0 , 
-               ( struct sockaddr*)&server_addr, &server_addr_size);
+      ( struct sockaddr*)&server_addr, &server_addr_size);
       printf( "receive: %s\n", buff_rcv);
       */
    }
    close( sock);
-   
+
    return 0;
 }
--- server.c
+++ server.c
@@ -10,47 +10,71 @@
 
 #define  BUFF_SIZE   1024
 
+typedef struct _person {
+  int currentFloor; //current floor for person
+  int toGoFloor; //floor to go for person
+  struct _person *person_next;
+} person;
+
+typedef person *person_ptr;
+
+typedef struct {
+  int man_flag;   //사람이 탔는지(1) 안탔는지(0)
+  int direction_flag; //위로가는지(1) 아래로가는지(0)
+  int now;      //현재 층
+  int togo;     //목적층
+  int error;    //고장횟수
+  person_ptr person_head;
+} elevator;
+
+#define TRUE 1
+#define FALSE 0
+
 /*
- * Protocol
- * length : 3Byte 
- * Author : Suchang Ko
- * Date   : 2016.06.08
- * ----------------
- * 1. 상/하 이동
- * [0]Byte : 1
- * [1]Byte : 현재 층 수
- * [2]Byte : 목적 층 수
- * 비고 : 목적 층 수는 0층(지하1층)~10층 범위에서 Random.
- * ----------------
- * 2. 시작/일시정지
- * [0]Byte : 2
- * [1]Byte : Not Used
- * [2]Byte : Not Used
- * 비고 : 시작상태에서 이 명령이 들어오면 일시정지, 
- * 일시정지 상태에서 이 명령이 들어오면 시작
- * ----------------
- * 3. 속도 조절
- * [0]Byte : 3
- * [1]Byte : 0 -> 0.5s | 1 -> 1s | 2 -> 2s 
- * [2]Byte : Not Used
- * 비고 : 없음
- * ----------------
- * 4. 수리
- * [0]Byte : 4
- * [1]Byte : Not Used
- * [2]Byte : Not Used
- * 비고 : 수리 명령을 받으면 Server측의 현재 고장도 초기화
- * ----------------
- * 5. 종료
- * [0]Byte : 5
- * [1]Byte : Not Used
- * [2]Byte : Not Used
- * 비고 : 프로그램을 종료한다.
- * ----------------
- */
+* Protocol
+* length : 3Byte 
+* Author : Suchang Ko
+* Date   : 2016.06.08
+* ----------------
+* 1. 상/하 이동
+* [0]Byte : 1
+* [1]Byte : 현재 층 수
+* [2]Byte : 목적 층 수
+* 비고 : 목적 층 수는 0층(지하1층)~10층 범위에서 Random.
+* ----------------
+* 2. 시작/일시정지
+* [0]Byte : 2
+* [1]Byte : Not Used
+* [2]Byte : Not Used
+* 비고 : 시작상태에서 이 명령이 들어오면 일시정지, 
+* 일시정지 상태에서 이 명령이 들어오면 시작
+* ----------------
+* 3. 속도 조절
+* [0]Byte : 3
+* [1]Byte : 0 -> 0.5s | 1 -> 1s | 2 -> 2s 
+* [2]Byte : Not Used
+* 비고 : 없음
+* ----------------
+* 4. 수리
+* [0]Byte : 4
+* [1]Byte : Not Used
+* [2]Byte : Not Used
+* 비고 : 수리 명령을 받으면 Server측의 현재 고장도 초기화
+* ----------------
+* 5. 종료
+* [0]Byte : 5
+* [1]Byte : Not Used
+* [2]Byte : Not Used
+* 비고 : 프로그램을 종료한다.
+* ----------------
+*/
 
 
-
+void init(elevator *elevator_ptr) {
+  elevator_ptr->now = 1;
+  elevator_ptr->togo = 1;
+  elevator_ptr->person_head = NULL;
+}
 
 void delay1(clock_t n)
 {
@@ -58,80 +82,163 @@ void delay1(clock_t n)
   while(clock() - start < n);
 }
 
-int   main( void)
+void show(char buff_rcv[])
 {
-   int   sock;
-   int   client_addr_size;
+  /*
+  // boolean elevatorExists, elevatorTaken, elevatorUp
+  // must be initiallized.
+  // boolean values are from elevator Structure
+  // ex. elevator->elevatorExists
 
-   struct sockaddr_in   server_addr;
-   struct sockaddr_in   client_addr;
+  // floor 0 is floor -1
+  int i, j;
+  for (i = 10; i >= 0; i--) {
+    //building ceiling with Floor mark.
+    printf("╠════════════════════╬════════════════════╬════════════════════╣ %dF\n", i);
 
-   char   buff_rcv[BUFF_SIZE+5];
-   char   buff_snd[BUFF_SIZE+5];
+    //elevator top part
+    for (j = 0; j<3; j++) {
+      printf("║");
+      if (elevatorExists) {
+        //If there is person in elevator, then elevator is taken(elevatorTaken is true).
+        printf("┌──────────────────┐");
+      }
+      else {
+        printf("                    ");
+      }
+    }
+    printf("║\n");
 
+    //elevator middle part
+    for (j = 0; j<3; j++) {
+      printf("║");
+      if (elevatorExists) {
+        //If there is person in elevator, then elevator is taken(elevatorTaken is true).
+        if (elevatorTaken) {
+          //When elevator is going up, elevatorUp is true.
+          if (elevatorUp) {
+            printf("|(FULL!)(%dF)( UP )|", floorNum);
+          }
+          else {
+            printf("|(FULL!)(%dF)(DOWN)|", floorNum);
+          }
+        }
+        else {
+          if (elevatorUp) {
+            printf("|(EMPTY)(%dF)( UP )|", floorNum);
+          }
+          else {
+            printf("|(EMPTY)(%dF)(DOWN)|", floorNum);
+          }
+        }
+      }
+      else {
+        printf("                    ");
+      }
+    }
+    printf("║\n");
 
+    //elevator bottom part
+    for (j = 0; j<3; j++) {
+      printf("║");
+      if (elevatorExists) {
+        //If there is person in elevator, then elevator is taken(elevatorTaken is true).
+        printf("└──────────────────┘");
+      }
+      else {
+        printf("                    ");
+      }
+    }
+    printf("║\n");
 
-   sock  = socket( PF_INET, SOCK_DGRAM, 0);
-   fcntl(sock, F_SETFL, O_NONBLOCK);
-   if( -1 == sock)
-   {
-      printf( "socket 생성 실패n");
-      exit( 1);
-   }
+    //building bottom
+    printf("╠════════════════════╬════════════════════╬════════════════════╣\n");
+    */
+  }
+}
 
-   memset( &server_addr, 0, sizeof( server_addr));
-   server_addr.sin_family     = AF_INET;
-   server_addr.sin_port       = htons( 4000);
-   server_addr.sin_addr.s_addr= htonl( INADDR_ANY);
+int main( void)
+{
+  int   sock;
+  int   client_addr_size;
 
-   if( -1 == bind( sock, (struct sockaddr*)&server_addr, sizeof( server_addr) ) )
-   {
-      printf( "bind() 실행 에러n");
-      exit( 1);
-   }
+  struct sockaddr_in   server_addr;
+  struct sockaddr_in   client_addr;
 
-   while( 1)
-   {
+  char   buff_rcv[BUFF_SIZE+5];
+  char   buff_snd[BUFF_SIZE+5];
+
+  //View
+  int   viewFlag;
+
+  sock  = socket( PF_INET, SOCK_DGRAM, 0);
+  fcntl(sock, F_SETFL, O_NONBLOCK);
+  if( -1 == sock)
+  {
+    printf( "socket 생성 실패n");
+    exit( 1);
+  }
+
+  memset( &server_addr, 0, sizeof( server_addr));
+  server_addr.sin_family     = AF_INET;
+  server_addr.sin_port       = htons( 4000);
+  server_addr.sin_addr.s_addr= htonl( INADDR_ANY);
+
+  if( -1 == bind( sock, (struct sockaddr*)&server_addr, sizeof( server_addr) ) )
+  {
+    printf( "bind() 실행 에러n");
+    exit( 1);
+  }
+
+  while( 1)
+  {
+    if(viewFlag == 0) {
       client_addr_size  = sizeof( client_addr);
       int i = recvfrom( sock, buff_rcv, BUFF_SIZE, 0 , 
-                     ( struct sockaddr*)&client_addr, &client_addr_size);
+        ( struct sockaddr*)&client_addr, &client_addr_size);
       //clearScreen();
-        if(i > 1){
-            system("clear");
-            switch(buff_rcv[0]){
-                case 1:{
-                    printf("[이동] %d층 -> %d층 \n",buff_rcv[1],buff_rcv[2]);
-                    break;
-                }
-                case 2:{
-                    if(buff_rcv[1] == 1){
-                        printf("[일시정지]\n");
-                    }else{
-                        printf("[시작]\n");
-                    }
-                    break;
-                }
-                case 3:{
-                    printf("[속도 조절] %d단계\n",buff_rcv[1]);
-                    break;
-                }
-                case 4:{
-                    printf("[수리]\n");
-                    break;
-                }
-                case 5:{
-                    printf("[종료]\n");
-                    return 0;
-                    break;
-                }
+      if(i > 1){
+        system("clear");
+          switch(buff_rcv[0]){
+          case 1:{
+            printf("[이동] %d층 -> %d층 \n",buff_rcv[1],buff_rcv[2]);
+            break;
+          }
+          case 2:{
+            if(buff_rcv[1] == 1){
+              printf("[일시정지]\n");
+            }else{
+              printf("[시작]\n");
             }
-            //printf( "1eceive: %d %s %d \n",i, buff_rcv,strlen(buff_rcv));
-
-          
-            sprintf( buff_snd, "%s%s", buff_rcv, buff_rcv);
-            sendto( sock, buff_snd, strlen( buff_snd)+1, 0,  // +1: NULL까지 포함해서 전송
-                         ( struct sockaddr*)&client_addr, sizeof( client_addr)); 
+            break;
+          }
+          case 3:{
+            printf("[속도 조절] %d단계\n",buff_rcv[1]);
+            break;
+          }
+          case 4:{
+            printf("[수리]\n");
+            break;
+          }
+          case 5:{
+            printf("[종료]\n");
+            return 0;
+            break;
+          }
+        }
+        //printf( "1eceive: %d %s %d \n",i, buff_rcv,strlen(buff_rcv));
+        sprintf( buff_snd, "%s%s", buff_rcv, buff_rcv);
+        sendto( sock, buff_snd, strlen( buff_snd)+1, 0,  // +1: NULL까지 포함해서 전송
+          ( struct sockaddr*)&client_addr, sizeof( client_addr)); 
+      }
+      viewFlag = 1;
+    } else {
+      //View
+      while(1) {
+        show(buff_rcv);
+        delay1(1000);
+      }
+      viewFlag = 0;
     }
-      delay1(100000);
-   }
+  }
 }
