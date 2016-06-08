1766cf2bdf29aeb1af0178fdec4127c658cee123
youjinwook <nugisquare@gmail.com>
2016-06-09 05:06:05 +0900

    add tts

--- client.c
+++ client.c
@@ -69,6 +69,14 @@ int mygetch(void)
    return ch;
 }
 
+void tts(char msg[]) {
+  char buff[256];
+  sprintf(buff, "pico2wave -w test.wav \"%s\"", msg);
+  system(buff);
+  system("aplay -q test.wav");
+  memset(buff, '\0', 256);
+}
+
 int   main( int argc, char **argv)
 {
    system("clear");
@@ -97,26 +105,26 @@ int   main( int argc, char **argv)
       char cmd_msg[3] = {0,0,0};
 
       printf("=================\n");
-      printf("[명령어를 입력해주세요]\n");
-      printf("[1] 이동\n");
-      printf("[2] 시작 / 일시정지\n");
-      printf("[3] 속도 조절\n");
-      printf("[4] 수리\n");
-      printf("[5] 종료\n");
+      printf("[명령어를 입력해주세요]\n");    tts("Insert Command.");
+      printf("[1] 이동\n");               tts("1, Move.");
+      printf("[2] 시작 / 일시정지\n");      tts("2, Pause or Resume.");
+      printf("[3] 속도 조절\n");           tts("3, Adjust Velocity.");
+      printf("[4] 수리\n");               tts("4, Repair.");
+      printf("[5] 종료\n");               tts("5, Exit.");
       printf("=================\n");
-      printf("[INPUT] : ");
+      printf("[INPUT] : ");              tts("Input your number.");
       scanf("%d",&cmd);
       switch(cmd){
       case 1:{ // 상하 이동
          int current_floor, target_floor;
          int direction;
-         printf("현재 몇 층에 있나요? (지하1층 -> -1 입력) : ");
+         printf("현재 몇 층에 있나요? (지하1층 -> -1 입력) : "); tts("Which floor are you in?");
          scanf("%d",&current_floor);
          if(current_floor == 0){
             printf("0층은 지하 1층으로 자동 변경됩니다.\n");
             current_floor = -1;
          }
-         printf("올라가기[1] 내려가기[2] : ");
+         printf("올라가기[1] 내려가기[2] : "); tts("Input 1 for going up or Input 2 for going down.");
          scanf("%d",&direction);
 
          srand(time(NULL));
@@ -139,9 +147,9 @@ int   main( int argc, char **argv)
 
          printf("[%d층] -> [%d층] 으로 이동\n",current_floor,target_floor);
          if(current_floor > target_floor){
-            printf("내려갑니다\n");   
+            printf("내려갑니다\n");   tts("Going down.");
          }else if(current_floor < target_floor){
-            printf("올라갑니다\n");   
+            printf("올라갑니다\n");   tts("Going up.");
          }
 
          cmd_msg[0] = 1;
@@ -157,7 +165,7 @@ int   main( int argc, char **argv)
       }
       case 3:{ // 속도 조절
          int target_speed;
-         printf("1 : 0.5sec | 2 : 1sec | 3 : 2sec\n");
+         printf("1 : 0.5sec | 2 : 1sec | 3 : 2sec\n"); tts("Input 1 for 0.5 seconds, Input 2 for 1 second, or Input 3 for 2 seconds.");
          printf("엘레베이터 속도를 입력하세요 : ");
          scanf("%d",&target_speed); 
          cmd_msg[0] = 3;
